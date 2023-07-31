807c61eae583c1441cbec47eb2361be404e4495b
ValentinMarinov <valio6663@abv.bg>
2023-07-31 21:24:01 +0300

    Add files via upload

--- ChromeExtansion.js
+++ ChromeExtansion.js
@@ -1,39 +1,47 @@
 let myLeads = []
-
-const inputEL = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
-console.log(ulEl)
-
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEL.value)
-    inputEL.value = ""
-    return renderLeads()
-})
-
-function renderLeads(){
-
-    let listItem = ""
-
-    for (let i = 0; i < myLeads.length; i ++ ){
-        listItem += 
-        `<li>
-            <a target='_blank' href = '${myLeads[i]}'> 
-            ${myLeads[i]}</a>
-        </li>`
-        console.log(listItem)
-    }
-
-    ulEl.innerHTML = listItem
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
 
-/*
-const recepit = "James"
-const sender = "Valio"
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
-const email = `Hey  ${recepit} ! nqkvi typotii. Cheers ${sender}`
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
 
-console.log(email)
-*/
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
